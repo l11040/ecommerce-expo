4bfb87cbcc9daa363d184bb297925f353886ff07
l11040 <l1104033837@gmail.com>
2026-01-26 23:29:30 +0900

    feat: WebView URI 개발/프로덕션 환경 분기 처리 추가
    - expo-constants를 활용하여 개발 서버 IP 동적 추출
    - __DEV__ 플래그로 환경별 URI 분기 (개발: 로컬 IP, 프로덕션: 서버 IP)

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
+import Constants from "expo-constants";
 import { StyleSheet, View } from "react-native";
 import { WebView } from "react-native-webview";
+
+const devServerIP = Constants.expoConfig?.hostUri?.split(":")[0];
+const uri = __DEV__ ? `http://${devServerIP}:3000` : "http://13.209.80.253";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WebView
-        source={{ uri: "http://localhost:3000" }}
-        style={styles.webview}
-      />
+      <WebView source={{ uri }} style={styles.webview} />
     </View>
   );
 }
