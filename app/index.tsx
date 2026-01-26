247575338fbd81d0569f100e6daa4a9cb814308a
이종윤 <rio@ijong-yun-ui-MacBookPro.local>
2026-01-26 22:23:27 +0900

    init

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: "http://localhost:3000" }}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
